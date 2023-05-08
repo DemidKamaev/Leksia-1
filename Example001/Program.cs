b822b9deb21ad70e5c308ce1afb75006b3107713
Демид <demid.kamaev@yandex.ru>
2023-05-08 18:45:17 +0300

    Add program number

---
+++ Example001/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+int numberA = 30;
+int numberB = 5;
+int results = numberA + numberB;
+System.Console.WriteLine(results);
